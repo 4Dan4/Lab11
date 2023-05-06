58a93fba75202dc0da518542210a8bdad3238fe3
Daniil <pinigindaniil4@gmail.com>
2023-05-06 15:35:00 +0300

    small fix

--- main.c
+++ main.c
@@ -2,10 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ftw.h>
+#include <getopt.h>
 
 const char *target_sequence; // глобальная переменная для хранения целевой последовательности
+void print_version() {
+    printf("Version 1.0\n"); // здесь можно указать актуальную версию программы
+    printf("Author: Daniil Pinigin\n");
+    printf("Group: N32452\n");
+    printf("Lab: #1, variant 3\n");
+}
 
-int search_file(const char *filepath, const struct stat *file_info, int typeflag, struct FTW *ftwbuf) {
+void print_help() {
+    printf("Usage: search_sequence [OPTIONS] directory target_sequence\n");
+    printf("Search for files containing the specified byte sequence.\n");
+    printf("\n");
+    printf("Options:\n");
+    printf("-v, --version     Print version information and exit.\n");
+    printf("-h, --help        Print this help message and exit.\n");
+}
+
+int search_file(const char *filepath, const struct stat *file_info, int typeflag) {
     if (typeflag == FTW_F) { // если это файл (а не директория)
         FILE *fp;
         char buffer[1024];
@@ -35,12 +51,35 @@ int search_file(const char *filepath, const struct stat *file_info, int typeflag
 }
 
 int main(int argc, char *argv[]) {
+    if (getenv("LAB11DEBUG") != NULL) {
+        printf("Debug option\n");
+    }
+
+    int option_index = 0;
+    static struct option long_options[] = {
+            {"version", no_argument, 0, 'v'},
+            {"help", no_argument, 0, 'h'},
+            {0, 0, 0, 0}
+    };
+    int c;
+    while ((c = getopt_long(argc, argv, "vh", long_options, &option_index)) != -1) {
+        switch (c) {
+            case 'v':
+                print_version();
+                return 0;
+            case 'h':
+                print_help();
+                return 0;
+            case '?':
+                printf("Unknown option: %s\n", argv[optind - 1]);
+                return 1;
+        }
+    }
     if (argc != 3) { // если количество аргументов неверно
         printf("Usage: %s directory target_sequence\n", argv[0]);
         return 1;
     }
     target_sequence = argv[2]; // сохраняем целевую последовательность в глобальной переменной
-    int flags = FTW_PHYS | FTW_DEPTH; // флаги для функции ftw()
     if (ftw(argv[1], search_file, 10) == -1) { // рекурсивно обходим указанный каталог и его подкаталоги
         printf("Error searching directory\n");
         return 1;
